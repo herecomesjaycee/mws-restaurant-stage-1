80f56b2e54c1a6f826cd8c6cc491c3431edb1479
herecomesjaycee <jaycee.cheong04@gmail.com>
2018-04-15 15:20:12 +0100

    Features
    - Added SW integration
    - Added meta tag charset

---
+++ js/sw.js
@@ -0,0 +1,27 @@
+self.addEventListener("install", function(event) {
+    var urlsToCache = [
+        "/",
+        "/js/dbhelper.js",
+        "/js/index.js",
+        "/js/main.js",
+        "/js/restaurant_info.js",
+        "/data/restaurants.json",
+        "/css/styles.css",
+        "/index.html",
+        "/restaurant.html"
+    ];
+    event.waitUntil(
+        caches.open("restaurant-static").then(function(cache) {
+            cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) return response;
+            return fetch(event.request);
+        })
+    );
+});
